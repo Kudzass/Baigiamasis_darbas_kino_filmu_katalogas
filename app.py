56ceca04f1d139380a9f41a43ccd99a741a3b446
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-04-09 22:42:18 +0300

    Pridėjau filmų pridėjimo formą ir pridėjimo funkcionalumą

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from models import db, Filmas, Zanras
-from flask import Flask, render_template, request
+from datetime import datetime
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmai.db'
@@ -8,19 +9,32 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
-@app.route('/')
-def index():
-    search = request.args.get('search')
+@app.route('/prideti', methods=['GET', 'POST'])
+def prideti_filma():
+    if request.method == 'POST':
+        pavadinimas = request.form['pavadinimas']
+        zanras_id = request.form['zanras_id']
+        rezisierius = request.form['rezisierius']
+        isleidimo_data = request.form['isleidimo_data']
+        imdb = request.form['imdb']
 
-    if search:
-        filmai = Filmas.query.filter(
-            Filmas.pavadinimas.ilike(f'%{search}%') |
-            Filmas.rezisierius.ilike(f'%{search}%')
-        ).all()
-    else:
-        filmai = Filmas.query.all()
+        try:
+            isleidimo_data = datetime.strptime(isleidimo_data, '%Y-%m-%d')
+        except ValueError:
+            isleidimo_data = None
 
-    return render_template('index.html', filmai=filmai)
+        naujas_filmas = Filmas(
+            pavadinimas=pavadinimas,
+            zanras_id=zanras_id,
+            rezisierius=rezisierius,
+            isleidimo_data=isleidimo_data,
+            imdb=imdb
+        )
+        db.session.add(naujas_filmas)
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('add.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
