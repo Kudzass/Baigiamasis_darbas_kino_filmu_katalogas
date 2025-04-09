2dd2cc22c5f5a5fc6e435719b4ce3b6a410122e6
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-04-09 22:29:59 +0300

    Pridėjau paieškos funkcionaluma ir sujungiau su pagrindiniu puslapiu

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from models import db, Filmas, Zanras
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmai.db'
@@ -9,8 +10,17 @@ db.init_app(app)
 
 @app.route('/')
 def index():
-    visi_filmai = Filmas.query.all()
-    return render_template('index.html', filmai=visi_filmai)
+    search = request.args.get('search')
+
+    if search:
+        filmai = Filmas.query.filter(
+            Filmas.pavadinimas.ilike(f'%{search}%') |
+            Filmas.rezisierius.ilike(f'%{search}%')
+        ).all()
+    else:
+        filmai = Filmas.query.all()
+
+    return render_template('index.html', filmai=filmai)
 
 if __name__ == '__main__':
     app.run(debug=True)
