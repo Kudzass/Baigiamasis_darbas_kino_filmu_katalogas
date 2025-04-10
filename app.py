f3dfe5e1b4c30644c5bc3a15202c87d821ea6707
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-04-10 19:47:42 +0300

    Paleidau testa – bandziau prideti filma bet nesuveiki grazinimas i pradini puslapi

--- app.py
+++ app.py
@@ -2,12 +2,25 @@ from flask import Flask, render_template, request, redirect, url_for
 from models import db, Filmas, Zanras
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///filmai.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
+
+@app.route('/')
+def index():
+    search = request.args.get('search')
+
+    if search:
+        filmai = Filmas.query.filter(
+            Filmas.pavadinimas.ilike(f'%{search}%') |
+            Filmas.rezisierius.ilike(f'%{search}%')
+        ).all()
+    else:
+        filmai = Filmas.query.all()
+
+    return render_template('index.html', filmai=filmai)
 
 @app.route('/prideti', methods=['GET', 'POST'])
 def prideti_filma():
@@ -19,9 +32,14 @@ def prideti_filma():
         imdb = request.form['imdb']
 
         try:
-            isleidimo_data = datetime.strptime(isleidimo_data, '%Y-%m-%d')
+            isleidimo_data = datetime.strptime(isleidimo_data, '%Y')
         except ValueError:
             isleidimo_data = None
+
+        try:
+            imdb = float(imdb)
+        except ValueError:
+            imdb = 0.0
 
         naujas_filmas = Filmas(
             pavadinimas=pavadinimas,
