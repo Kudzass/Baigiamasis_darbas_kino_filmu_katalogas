78af432766c8682a0c6cb70361b9be5b3804c1c0
Name Surname“Tomas Sipaitisgit config --global user.name Tomas <t.sipaitis@gmail.com>
2025-04-10 20:39:15 +0300

    sukuriau edit.html filmu redagavimui

--- app.py
+++ app.py
@@ -8,10 +8,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 
+
 @app.route('/')
 def index():
     search = request.args.get('search')
-
     if search:
         filmai = Filmas.query.filter(
             Filmas.pavadinimas.ilike(f'%{search}%') |
@@ -19,8 +19,8 @@ def index():
         ).all()
     else:
         filmai = Filmas.query.all()
-
     return render_template('index.html', filmai=filmai)
+
 
 @app.route('/prideti', methods=['GET', 'POST'])
 def prideti_filma():
@@ -53,6 +53,32 @@ def prideti_filma():
         return redirect(url_for('index'))
 
     return render_template('add.html')
+
+
+@app.route('/redaguoti/<int:id>', methods=['GET', 'POST'])
+def redaguoti_filma(id):
+    filmas = Filmas.query.get_or_404(id)
+
+    if request.method == 'POST':
+        filmas.pavadinimas = request.form['pavadinimas']
+        filmas.zanras_id = request.form['zanras_id']
+        filmas.rezisierius = request.form['rezisierius']
+
+        try:
+            filmas.imdb = float(request.form['imdb'])
+        except ValueError:
+            filmas.imdb = 0.0
+
+        try:
+            filmas.isleidimo_data = datetime.strptime(request.form['isleidimo_data'], '%Y-%m-%d')
+        except ValueError:
+            filmas.isleidimo_data = None
+
+        db.session.commit()
+        return redirect(url_for('index'))
+
+    return render_template('edit.html', filmas=filmas)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
